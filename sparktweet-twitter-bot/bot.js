999ace1de01a7135ff6d7ff340ffbad1bc280b22
Kristina <kdurivage@gelicia.com>
2015-01-03 19:01:32 -0600

    Work on tweet queue, coming from mentions

--- sparktweet-twitter-bot/bot.js
+++ sparktweet-twitter-bot/bot.js
@@ -1,36 +1,79 @@
 // Our Twitter library
 var Twit = require('twit');
 var rest = require('restler');
+var levelup = require('level');
+var q = require('q');
 
 // We need to include our configuration file
 var T = new Twit(require('./twitterConfig.js'));
 var sparkConfig = require('./sparkConfig.js');
-
-var userName = '';
-T.get('account/verify_credentials', {}, function(error, data){
-	userName = data.screen_name;
-});
+var displayed_db = levelup('./displayedTweets');
 
 var tweetQueue = [];
 
-function findTweetToDisplay() {
+var userName = 'tweetSkirt';
+
+function queueTweets() {
 	console.log("looking for mentions...");
-	//get the 10 latest mentions
-	T.get('statuses/mentions_timeline', {count:10}, function (error, data) {
-		
-		
-			//send an api call to spark
-			/*rest.post('https://api.spark.io/v1/devices/' + sparkConfig.deviceID + '/sing', {
-				data: { 'access_token': sparkConfig.accessToken,
-				'args': music.notes }
-			}).on('complete', function(data, response) {
-				console.log("played " + music.notes );
-				//when it's done, favorite the tweet so the person knows they are now a performer
-				T.post('favorites/create', { id: music.playID }, function(err, reply) {});
-			});*/
-		
+	var now = new Date(); 
+	//get the 50 latest mentions
+	T.get('statuses/mentions_timeline', {count:50}, function (error, data) {
+		for (var i = 0; i < data.length; i++) {
+			var dataOfInterest = data[i];
+			//only look for tweets less than 24 hours old
+			var tweetDate = new Date(dataOfInterest.created_at);
+			if(now - tweetDate < (1000*60*60*24)){
+				//check that the tweet has not yet been displayed
+				//we send all the data to it because the loop will keep running and we want to keep the data attached to the promise
+				var isDisplayedPromise = isAlreadyDisplayed(dataOfInterest);
+
+				isDisplayedPromise.done(function(result){
+					if(result.toQueue){//tweet not found! queue it up!
+						var queueTweet = {
+							"id" : result.data.id,
+							message : "@" + result.data.user.screen_name + " - " + (result.data.text).substring(userName.length+2)
+						};
+						tweetQueue.push(queueTweet);
+					}
+				});
+			}
+		};
 	});
+
+	//get usages of the #tweetSkirt hashtag
 }
 
-findTweetToDisplay(); 
-setInterval(findTweetToDisplay, 1000 * 60);
+function isAlreadyDisplayed(tweetData){
+	var deferred = q.defer();
+	displayed_db.get(tweetData.id, function (err, value) {
+		if (err) {
+			if (err.notFound){
+				deferred.resolve({toQueue: true, data:tweetData});
+			}
+			else {
+				//count it as displayed (will not queue up) if there's an error, it should get queued next time around
+				deferred.resolve({toQueue: false, data:tweetData});
+			}
+			
+		}
+		else {
+			deferred.resolve({toQueue: false, data:tweetData});
+		}
+	});
+
+	return deferred.promise;
+}
+
+function displayTweet(){
+	if (tweetQueue.length > 0){
+		var tweet = tweetQueue.pop();
+		console.log("tweet displayed ", tweet);
+	}
+}
+
+queueTweets(); 
+//find tweets every three minutes
+setInterval(queueTweets, 3 * 1000 * 60);
+
+//display a tweet every minute
+setInterval(displayTweet, 1000 * 60);
