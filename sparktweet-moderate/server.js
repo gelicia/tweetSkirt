0eeb123a1a302980a847180b910dfa45d62d8059
Kristina <kdurivage@gelicia.com>
2015-02-08 22:24:08 -0600

    Create the server call to get the tweetQueue. This will be used by the moderation app.

--- sparktweet-moderate/server.js
+++ sparktweet-moderate/server.js
@@ -1,16 +1,19 @@
 var express = require('express');
-var levelup = require('level');
+var Datastore = require('nedb');
 var app = express();
 
-var tweetQueue_db = levelup('./tweetQueue');
-
 app.get('/tweetQueue', function (req, res) {
-	var tweets = [];
+	var tweetQueue_db = new Datastore({filename: '../sparktweet-twitter-bot/tweetQueue.db', autoload:true});
+	tweetQueue_db.find({}, function (err, docs) {
+		res.send(docs);
+	});
+});
 
-	tweetQueue_db.createReadStream()
-	.on('data', function (data) {
-		tweets = data.value;
-	})
+var server = app.listen(3000, function () {
 
-  //res.send('about')
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port)
+
 })
