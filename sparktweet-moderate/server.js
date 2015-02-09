6e1db2f6a6327fd92741d9dd04faeab9af72687a
Kristina <kdurivage@gelicia.com>
2015-02-09 15:09:13 -0600

    Some other time, react...

--- sparktweet-moderate/server.js
+++ sparktweet-moderate/server.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var Datastore = require('nedb');
+
+var loginCreds = require('./loginConfig');
+
 var app = express();
 
 app.get('/tweetQueue', function (req, res) {
@@ -7,6 +10,16 @@ app.get('/tweetQueue', function (req, res) {
 	tweetQueue_db.find({}, function (err, docs) {
 		res.send(docs);
 	});
+});
+
+app.get('/login', function (req, res) {
+
+	//loginCreds.salt;
+	//loginCreds.md5Password;
+
+	console.log(req.query.username);
+	console.log(req);
+	res.send();
 });
 
 var server = app.listen(3000, function () {
